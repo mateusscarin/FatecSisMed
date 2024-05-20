ae48554c6f18aa0893d7041b69c1b1c8e9cf0d8c
mateusscarin <mateus.scarin@gmail.com>
2024-05-20 20:30:06 -0300

    feat: aula 20/05/2024

--- FatecSisMed.IdentityServer/SeedDataBase/Entities/DataBaseIdentityServerInitializer.cs
+++ FatecSisMed.IdentityServer/SeedDataBase/Entities/DataBaseIdentityServerInitializer.cs
@@ -1,5 +1,72 @@
-﻿namespace FatecSisMed.IdentityServer.SeedDataBase.Entities;
+﻿using FatecSisMed.IdentityServer.Configuration;
+using FatecSisMed.IdentityServer.Data.Entities;
+using FatecSisMed.IdentityServer.SeedDataBase.Interfaces;
+using IdentityModel;
+using Microsoft.AspNetCore.Identity;
+using System.Security.Claims;
 
-public class DataBaseIdentityServerInitializer
+namespace FatecSisMed.IdentityServer.SeedDataBase.Entities;
+
+public class DataBaseIdentityServerInitializer : IDataBaseInitializer
 {
+    private readonly UserManager<ApplicationUser> _userManager;
+    private readonly RoleManager<IdentityRole> _roleManager;
+
+    public DataBaseIdentityServerInitializer(UserManager<ApplicationUser> userManager,
+        RoleManager<IdentityRole> roleManager)
+    {
+        _userManager = userManager;
+        _roleManager = roleManager;
+    }
+
+    public void InitializeSeedRoles()
+    {
+        if (!_roleManager.RoleExistsAsync(IdentityConfiguration.Admin).Result)
+        {
+            IdentityRole roleAdmin = new IdentityRole();
+            roleAdmin.Name = IdentityConfiguration.Admin;
+            roleAdmin.NormalizedName = IdentityConfiguration.Admin.ToUpper();
+            _roleManager.CreateAsync(roleAdmin).Wait();
+        }
+        if (!_roleManager.RoleExistsAsync(IdentityConfiguration.Client).Result)
+        {
+            IdentityRole roleClient = new IdentityRole();
+            roleClient.Name = IdentityConfiguration.Client;
+            roleClient.NormalizedName = IdentityConfiguration.Client.ToUpper();
+            _roleManager.CreateAsync(roleClient).Wait();
+        }
+    }
+
+    public void InitializeSeedUsers()
+    {
+        if (_userManager.FindByEmailAsync("mateus.scarin@fatec.sp.gov.br").Result is null)
+        {
+            ApplicationUser admin = new ApplicationUser()
+            {
+                UserName = "scarin",
+                NormalizedUserName = "SCARIN",
+                Email = "mateus.scarin@fatec.sp.gov.br",
+                NormalizedEmail = "MATEUS.SCARIN@FATEC.SP.GOV.BR",
+                EmailConfirmed = true,
+                LockoutEnabled = false,
+                PasswordHash = "+55 (17) 99778-6147",
+                FirtName = "ADMIN",
+                LastName = "Scarin",
+                SecurityStamp = Guid.NewGuid().ToString()
+            };
+            IdentityResult resultAdmin = _userManager.CreateAsync(admin, "123Mudar*").Result;
+            if (resultAdmin.Succeeded)
+            {
+                _userManager.AddToRoleAsync(admin, IdentityConfiguration.Admin).Wait();
+                var adminClains = _userManager.AddClaimsAsync(admin, new Claim[]
+                {
+                    new Claim(JwtClaimTypes.Name, $"{admin.FirtName} {admin.LastName}"),
+                    new Claim(JwtClaimTypes.GivenName, admin.FirtName),
+                    new Claim(JwtClaimTypes.FamilyName, admin.LastName),
+                    new Claim(JwtClaimTypes.Role, IdentityConfiguration.Admin)
+                }).Result;
+            }
+        }
+    }
+
 }
--- FatecSisMed.IdentityServer/obj/Debug/net6.0/FatecSisMed.IdentityServer.AssemblyInfo.cs
+++ FatecSisMed.IdentityServer/obj/Debug/net6.0/FatecSisMed.IdentityServer.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FatecSisMed.IdentityServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9be0b4ff067f7dca45d1882525ea81ae4d45a86")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75dfce7c13d28d7d35ea9613db5ac8c8c7af2d76")]
 [assembly: System.Reflection.AssemblyProductAttribute("FatecSisMed.IdentityServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FatecSisMed.IdentityServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
